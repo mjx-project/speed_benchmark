6dbc0a925f4b476d9eba0a74ac566ff3cfbb1702
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-11-26 17:09:37 +0900

    update fig

--- results/plot.py
+++ results/plot.py
@@ -4,68 +4,44 @@ import matplotlib.pyplot as plt
 from numpy.core.fromnumeric import size
 import seaborn as sns
 # %%
-sns.set_style("white")
+# sns.set()
+sns.set_style("ticks")
+# sns.set_style("white")
+# sns.set_style("whitegrid")
+sns.set_context("paper", 1.9)
 
 labels = ['Pass agent', 'Rule-based agent']
 men_means = [8.874, 13.587]
 women_means = [1241.311, 13830.488]
 
-x = np.array([0, 0.4]) # np.arange(len(labels))  # the label locations
-width = 0.15 # the width of the bars
+x = np.array([2.0, 6.0]) # np.arange(len(labels))  # the label locations
+width = 1.2 # the width of the bars
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(x - width/2, men_means, width, label='Mjx')
+rects1 = ax.bar(x - width/2, men_means, width, label='Mjx (ours)')
 rects2 = ax.bar(x + width/2, women_means, width, label='Mjai')
 
 # remove frames
 right_side = ax. spines["right"]
-right_side. set_visible(False)
-right_side = ax. spines["top"]
-right_side. set_visible(False)
+right_side.set_visible(False)
+top_side = ax.spines["top"]
+top_side.set_visible(False)
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_yscale('log')
-ax.set_ylim((0, 5 * 10**4))
-ax.set_ylabel('log10(sec)')
+ax.set_ylabel('log10 (sec)', fontsize=16)
+ax.set_yticks([10, 100, 1000, 10000, 10**5])
+# ax.set_yticklabels([10, 100, 1000, 10000])
+# ax.set_yticklabels()
 # ax.set_title('Average time for 100 games')
-ax.set_xlim((-0.25, 0.65))
+ax.set_xlim((0.0, 8.0))
 ax.set_xticks(x)
-ax.set_xticklabels(labels)
+ax.set_xticklabels(labels, fontsize=15)
 ax.grid(axis="y", linestyle="dotted")
-ax.legend(frameon=False, loc=2)
+ax.legend(frameon=False, fontsize=16, ncol=2, loc='upper center', bbox_to_anchor=(0.5, 1.20))
 
-ax.bar_label(rects1, padding=3, size=13)
-ax.bar_label(rects2, padding=3, size=13)
-
-fig.tight_layout()
-
-plt.savefig("speed_benchmark.pdf")
-
-# %%
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-
-
-labels = ['G1', 'G2', 'G3', 'G4', 'G5']
-men_means = [20, 34, 30, 35, 27]
-women_means = [25, 32, 34, 20, 25]
-
-x = np.arange(len(labels))  # the label locations
-width = 0.35  # the width of the bars
-
-fig, ax = plt.subplots()
-rects1 = ax.bar(x - width/2, men_means, width, label='Men')
-rects2 = ax.bar(x + width/2, women_means, width, label='Women')
-
-# Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Scores')
-ax.set_title('Scores by group and gender')
-ax.set_xticks(x)
-ax.set_xticklabels(labels)
-ax.legend()
-
-
+# ax.bar_label(rects1, padding=3, size=13)
+# ax.bar_label(rects2, padding=3, size=13)
 def autolabel(rects):
     """Attach a text label above each bar in *rects*, displaying its height."""
     for rect in rects:
@@ -74,25 +50,13 @@ def autolabel(rects):
                     xy=(rect.get_x() + rect.get_width() / 2, height),
                     xytext=(0, 3),  # 3 points vertical offset
                     textcoords="offset points",
+                    fontsize=13,
                     ha='center', va='bottom')
-
 
 autolabel(rects1)
 autolabel(rects2)
 
 fig.tight_layout()
 
-plt.show()
-
-# %%
-sns.set()
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-titanic_dataset = sns.load_dataset("titanic")
-
-sns.barplot(x = "class", y = "age", hue = "embark_town", data = titanic_dataset)
-plt.show()
-# %%
-titanic_dataset
+plt.savefig("speed_benchmark.pdf")
 # %%
